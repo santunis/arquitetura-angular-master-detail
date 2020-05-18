9c04a406c39d39ab064c1e1c144e0375b18831b6
Santunis Monteiro de Oliveira <santunis.oliveira@fornecedores.sicoob.com.br>
2020-05-18 12:13:41 -0300

    Melhorias no EntryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,7 +3,7 @@ import { Injectable, Injector } from '@angular/core';
 import { CategoryService } from '../../categories/shared/category.service';
 import { Entry } from './entry.model';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
-import { flatMap } from 'rxjs/operators';
+import { flatMap, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -19,20 +19,20 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.create(entry);
-      })
-    );
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
         entry.category = category;
-        return super.update(entry)
-      })
+        return sendFn(entry)
+      }),
+      catchError(this.handleError)
     );
   }
   
